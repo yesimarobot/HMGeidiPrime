f660480abe57b16ffb1669cc69d430a9f7ba6048
Paul Schreiber <paulschreiber@pauls-macbook-pro.local>
2021-09-03 01:57:20 +0000

    UPDATE SIDE MENU TITLE

--- src/main/SideMenu.js
+++ src/main/SideMenu.js
@@ -1,63 +1,81 @@
-import { IonHeader, IonContent, IonToolbar, IonTitle, IonMenuToggle, IonItem, IonIcon, IonMenu, IonLabel, IonList, IonListHeader } from '@ionic/react';
+import {
+  IonHeader,
+  IonContent,
+  IonToolbar,
+  IonTitle,
+  IonMenuToggle,
+  IonItem,
+  IonIcon,
+  IonMenu,
+  IonLabel,
+  IonList,
+  IonListHeader,
+} from "@ionic/react";
 import { useSideMenu } from "../main/SideMenuProvider";
 
 import "../theme/SideMenu.css";
 
 const SideMenu = (props) => {
+  const { type = "overlay" } = props;
+  const mainContent = props.children;
+  const menuOptions = useSideMenu();
 
-	const { type = "overlay" } = props;
-	const mainContent = props.children;
-	const menuOptions = useSideMenu();
+  return (
+    <IonMenu
+      contentId={menuOptions.pageName}
+      side={menuOptions.side}
+      type={type}
+    >
+      <IonHeader>
+        <IonToolbar>
+          <IonTitle>HELLO MERCH</IonTitle>
+        </IonToolbar>
+      </IonHeader>
 
-	return (
-		<IonMenu contentId={ menuOptions.pageName } side={ menuOptions.side } type={ type }>
-			<IonHeader>
-				<IonToolbar>
-					<IonTitle>Menu</IonTitle>
-				</IonToolbar>
-			</IonHeader>
+      <IonContent forceOverscroll={false} id="main">
+        {mainContent}
 
-			<IonContent forceOverscroll={ false } id="main">
+        <IonListHeader>{menuOptions.pageName}</IonListHeader>
 
-				{ mainContent }
-
-				<IonListHeader>{ menuOptions.pageName }</IonListHeader>
-
-				{ menuOptions !== null &&
-					<IonList lines="none">
-						{ menuOptions && menuOptions.options.map((menuOption, i) => {
-							
-							if (menuOption.url === null) {
-
-								return (
-
-									<IonMenuToggle key={ i } autoHide={ true }>
-										<IonItem onClick={ menuOption.clickEvent } lines="none" detail={ false }>
-											<IonIcon slot="start" icon={ menuOption.icon } />
-											<IonLabel>{ menuOption.text }</IonLabel>
-										</IonItem>
-									</IonMenuToggle>
-								);
-							} else {
-
-								if (menuOption.url !== null) {
-									return (
-
-										<IonMenuToggle key={ i } autoHide={ true }>
-											<IonItem detail={ false } routerLink={ menuOption.url } lines="none">
-												<IonIcon slot="start" icon={ menuOption.icon } />
-												<IonLabel>{ menuOption.text }</IonLabel>
-											</IonItem>
-										</IonMenuToggle>
-									);
-								}
-							}
-						})}
-					</IonList>
-				}
-			</IonContent>
-		</IonMenu>
-	);
-}
+        {menuOptions !== null && (
+          <IonList lines="none">
+            {menuOptions &&
+              menuOptions.options.map((menuOption, i) => {
+                if (menuOption.url === null) {
+                  return (
+                    <IonMenuToggle key={i} autoHide={true}>
+                      <IonItem
+                        onClick={menuOption.clickEvent}
+                        lines="none"
+                        detail={false}
+                      >
+                        <IonIcon slot="start" icon={menuOption.icon} />
+                        <IonLabel>{menuOption.text}</IonLabel>
+                      </IonItem>
+                    </IonMenuToggle>
+                  );
+                } else {
+                  if (menuOption.url !== null) {
+                    return (
+                      <IonMenuToggle key={i} autoHide={true}>
+                        <IonItem
+                          detail={false}
+                          routerLink={menuOption.url}
+                          lines="none"
+                        >
+                          <IonIcon slot="start" icon={menuOption.icon} />
+                          <IonLabel>{menuOption.text}</IonLabel>
+                        </IonItem>
+                      </IonMenuToggle>
+                    );
+                  }
+                }
+              })}
+          </IonList>
+        )}
+      </IonContent>
+    </IonMenu>
+  );
+};
 
 export default SideMenu;
